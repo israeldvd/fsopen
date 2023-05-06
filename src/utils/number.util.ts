b0351eb3b4fc4cf573b234d2e1ea82a8d727eb61
Israel David <israelcaetano21@hotmail.com>
2023-05-06 19:21:32 -0300

    refactor: make function name more precise

--- src/utils/number.util.ts
+++ src/utils/number.util.ts
@@ -2,7 +2,7 @@ export const isNotNumber = (arg: any) => {
     return isNaN(Number(arg));
 };
 
-export const areNumbers = (args: any[]) => {
+export const checkIfNumbers = (...args: any[]) => {
     return args.length>0 && !args.reduce((prevValue, currValue) => {
         return isNotNumber(currValue) || prevValue;
     }, false);
