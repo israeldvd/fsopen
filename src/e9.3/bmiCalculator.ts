a51fb85250822bd12b593a6d9d7bbec5325e8780
Israel David <israelcaetano21@hotmail.com>
2023-05-06 18:31:38 -0300

    refactor: convert variable to constant

--- src/e9.3/bmiCalculator.ts
+++ src/e9.3/bmiCalculator.ts
@@ -18,7 +18,7 @@ function parseBMIInput(args: string[]): PersonInfoValues {
 }
 
 function calculateBmi(height: number, weight: number) {
-    let bmi = weight / (height / 100) ** 2;
+    const bmi = weight / (height / 100) ** 2;
 
     if (bmi < 18.5) return "Underweight";
     else if (bmi < 25) return "Healthy weight";
