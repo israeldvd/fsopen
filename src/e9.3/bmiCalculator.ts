2645ba8bea854879b562fc11d201545d5b5b5802
Israel David <israelcaetano21@hotmail.com>
2023-05-08 13:27:29 -0300

    fix(bmi): match message to specification

--- src/e9.3/bmiCalculator.ts
+++ src/e9.3/bmiCalculator.ts
@@ -26,7 +26,7 @@ export default function calculateBmi(height: number, weight: number) {
   const bmi = weight / (height / 100) ** 2;
 
   if (bmi < 18.5) return "Underweight";
-  else if (bmi < 25) return "Healthy weight";
+  else if (bmi < 25) return "Normal (healthy weight)";
   else if (bmi < 30) return "Overweight";
   else return "Obesity";
 }
