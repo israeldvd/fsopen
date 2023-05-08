363511eaab3f4766e86c439bcf9f8c5a4fe7338d
Israel David <israelcaetano21@hotmail.com>
2023-05-08 13:28:02 -0300

    feat: implement WebBMI

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
 import express from "express";
+import { areAllNumbers } from "./utils/number.util";
+import { BmiResponse } from "./types/responses.type";
+import calculateBmi from "./e9.3/bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
-const PORT = process.env.PORT || 3003;
+app.get("/bmi", (req, res) => {
+  const queryHeight = req.query?.height;
+  const queryWeight = req.query?.weight;
+  let height: number;
+  let weight: number;
+  let hasMalformedParameters = !areAllNumbers(queryHeight, queryWeight);
+  let healthCondition: string;
+  let response: BmiResponse;
+
+  try {
+    if (hasMalformedParameters) throw new Error("Input values must be numbers");
+    else {
+      height = Number(queryHeight);
+      weight = Number(queryWeight);
+    }
+
+    healthCondition = calculateBmi(height, weight);
+    response = { height, weight, bmi: healthCondition };
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+
+    if (error instanceof Error) {
+      errorMessage += " " + error.message;
+    }
+
+    console.error(errorMessage);
+    response = { error: "malformed parameters" };
+  }
+
+  res.json(response);
+});
+
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
