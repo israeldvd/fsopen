1257017341bf850d9f8d4317925a20716524242b
Israel David <israelcaetano21@hotmail.com>
2023-05-08 18:32:31 -0300

    refactor: define helpers for main calculators

--- src/e9.3/bmiCalculator.ts
+++ src/e9.3/bmiCalculator.ts
@@ -1,43 +1,7 @@
-import { areAllNumbers } from "../utils/number.util";
+import calculateBmi, {
+  calculateBMIByInput,
+} from "../helpers/bmi-calculator.helper";
 
-export interface PersonInfoValues {
-  weight: number;
-  height: number;
-}
+export { calculateBmi };
 
-export function parseBMIInput(args: string[]): PersonInfoValues {
-  if (args.length < 4) throw new Error("Too few arguments!");
-  if (args.length > 4) throw new Error("Too many arguments!");
-
-  const heightArgInput = args[2];
-  const weightArgInput = args[3];
-
-  if (!areAllNumbers(heightArgInput, weightArgInput)) {
-    throw new Error("Input values are not numbers.");
-  }
-
-  return {
-    height: Number(heightArgInput),
-    weight: Number(weightArgInput),
-  };
-}
-
-export default function calculateBmi(height: number, weight: number) {
-  const bmi = weight / (height / 100) ** 2;
-
-  if (bmi < 18.5) return "Underweight";
-  else if (bmi < 25) return "Normal (healthy weight)";
-  else if (bmi < 30) return "Overweight";
-  else return "Obesity";
-}
-
-try {
-  const { height, weight } = parseBMIInput(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
-  }
-  console.log(errorMessage);
-}
+calculateBMIByInput(process.argv);
