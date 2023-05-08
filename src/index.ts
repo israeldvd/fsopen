15481450509b645a52e06fc18faf41927a2818fc
Israel David <israelcaetano21@hotmail.com>
2023-05-08 18:44:16 -0300

    feat: implement WebExercises (e9.7)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
-import calculateBmi from "./e9.3/bmiCalculator";
-import { BmiResponse } from "./types/responses.type";
+import calculateBmi from "./helpers/bmi-calculator.helper";
+import {
+  BmiResponse,
+  ExercisesResultType,
+  WebExercisesResponse,
+} from "./types/responses.type";
 import { areAllNumbers } from "./utils/number.util";
+import exerciseCalculator from "./helpers/exercises-calculator.helper";
+
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
 app.get("/bmi", (req, res) => {
-  const queryHeight = req.query?.height;
-  const queryWeight = req.query?.weight;
+  const queryHeight = req.query?.height as string;
+  const queryWeight = req.query?.weight as string;
   let height: number;
   let weight: number;
   const hasMalformedParameters = !areAllNumbers(queryHeight, queryWeight);
@@ -38,6 +45,52 @@ app.get("/bmi", (req, res) => {
   }
 
   res.json(response);
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises: dailyExercises, target } = req.body;
+  const dailyExercisesNumbers: number[] = [];
+  let dailyExercisesResultInfo: ExercisesResultType = {
+    periodLength: 0,
+    average: 0,
+    rating: 0,
+    success: false,
+    target: 0,
+    trainingDays: 0,
+    ratingDescription: "",
+  };
+  const response: { body: WebExercisesResponse; status: number | undefined } = {
+    body: dailyExercisesResultInfo, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    status: undefined,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access
+  if (!target || !dailyExercises || dailyExercises.length != 7) {
+    response.body = { error: "parameters missing" };
+    response.status = 400;
+  } else if (
+    target &&
+    dailyExercises &&
+    areAllNumbers(target, ...dailyExercises) // eslint-disable-line @typescript-eslint/no-unsafe-argument
+  ) {
+    for (const val of dailyExercises) {
+      dailyExercisesNumbers.push(Number(val));
+    }
+
+    dailyExercisesResultInfo = exerciseCalculator(
+      dailyExercisesNumbers,
+      Number(target)
+    );
+
+    response.body = dailyExercisesResultInfo; // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    response.status = 200;
+  } else {
+    response.body = { error: "malformatted parameters" };
+    response.status = 400;
+  }
+
+  return res.status(response.status).json(response.body);
 });
 
 const PORT = process.env.PORT || 3002;
--- src/types/responses.type.ts
+++ src/types/responses.type.ts
@@ -1,5 +1,9 @@
+import { Result } from "../helpers/exercises-calculator.helper";
+
 export type BmiResponse =
   | { weight: number; height: number; bmi: string }
   | { error: string };
 
-export { Result as ExercisesResultType } from "../e9.3/exerciseCalculator";
+export { Result as ExercisesResultType } from "../helpers/exercises-calculator.helper";
+
+export type WebExercisesResponse = Result | { error: string };
