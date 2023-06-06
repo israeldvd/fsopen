b657795249ad39a43d0ab3ea303d5250bbb85b91
Israel David <israelcaetano21@hotmail.com>
2023-06-06 14:40:54 -0300

    feat(unicafe): display total number of feedback

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,45 @@
 import { useState } from "react";
 
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Feedback = ({ description, count }) => {
+  return (
+    <p>
+      {description} {count}
+    </p>
+  );
+};
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>Feedback page</div>;
+  const handleGoodFeedback = () => {
+    setGood(good + 1);
+  };
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBadFeedback = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="good" handleClick={handleGoodFeedback} />
+      <Button text="neutral" handleClick={handleNeutralFeedback} />
+      <Button text="bad" handleClick={handleBadFeedback} />
+
+      <h2>Statistics</h2>
+      <Feedback description={"good"} count={good} />
+      <Feedback description={"neutral"} count={neutral} />
+      <Feedback description={"bad"} count={bad} />
+    </div>
+  );
 };
 
 export default App;
