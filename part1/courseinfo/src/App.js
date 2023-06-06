944810282aad9ef0593a51f91b1a20db5ffe0f36
Israel David <israelcaetano21@hotmail.com>
2023-06-06 08:50:09 -0300

    refactor(e1.2): modify Content component
    Guarantee it does not render any names of parts or their number of exercises by itself.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,16 +1,16 @@
 const Header = (props) => <h1>{props.course}</h1>;
 
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercisesCount}
+  </p>
+);
+
 const Content = (props) => (
   <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part part={props.part1} exercisesCount={props.exercises1} />
+    <Part part={props.part2} exercisesCount={props.exercises2} />
+    <Part part={props.part3} exercisesCount={props.exercises3} />
   </>
 );
 
