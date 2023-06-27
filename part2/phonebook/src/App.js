0bab0d9889929b9beb6ceaf254c2756634a387ca
Israel David <israel-david@outlook.com.br>
2023-06-27 11:59:31 -0300

    feat(phonebook): allow adding a new person

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,33 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const person = {
+      name: newName
+    }
+
+    setPersons(persons.concat(person));
+    setNewName("");
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(event) => { setNewName(event.target.value); }} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (<li key={person.name}><p>{person.name}</p></li>))}
+      </ul>
     </div>
   );
 };
