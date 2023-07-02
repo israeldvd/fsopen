a20b8973ee0f901ddd9eb99080cd25ad5606ad94
Israel David <israel-david@outlook.com.br>
2023-07-02 18:04:13 -0300

    feat(phonebook): prevent adding already-existing names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,20 +8,32 @@ const App = () => {
     event.preventDefault();
 
     const person = {
-      name: newName
-    }
+      name: newName,
+    };
+
+    const personAlreadyExists = persons.some((person) => {
+      return person.name === newName;
+    });
+
+    if (personAlreadyExists)
+      return alert(`${newName} is already added to phonebook`);
 
     setPersons(persons.concat(person));
     setNewName("");
-  }
-
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={(event) => { setNewName(event.target.value); }} />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => {
+              setNewName(event.target.value);
+            }}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -29,7 +41,11 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (<li key={person.name}><p>{person.name}</p></li>))}
+        {persons.map((person) => (
+          <li key={person.name}>
+            <p>{person.name}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
