4eda5a3cb3d10f3a9c67305253ac07053a977ec5
Israel David <israel-david@outlook.com.br>
2023-07-30 13:43:47 -0300

    feat(phoneboo-b): show data sent in POST requests

--- part3/phonebook-backend/src/index.js
+++ part3/phonebook-backend/src/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("data", function getData(req) {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
