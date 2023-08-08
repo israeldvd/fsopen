a4973a9e72a85cd27109e699f04f36d284d1dd1e
Israel David <israel-david@outlook.com.br>
2023-08-08 19:07:29 -0300

    feat(phonebook-b): add test MongoDB database setup

--- part3/phonebook-backend/README.md
+++ part3/phonebook-backend/README.md
@@ -5,3 +5,12 @@ This repository contains the backend of the **phonebook** app.
 ## URI
 
 You can access it by and send requests to the URL <https://dvd-phonebook.fly.dev>.
+
+## Using environment variables
+
+Note that it might be necessary to set up environment variables in order to use the app correctly. Create or modify the `.env` to having the following:
+
+```shell
+PHONE_DB_HOSTNAME=phonebook.replacehere.mongodb.net
+PHONE_DB_USERNAME=username
+```
---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const [, , password, name, number] = process.argv;
+const nameIsMissing = !name || name.trim() === "";
+const numberIsMissing = !number || number.trim() === "";
+
+if (!nameIsMissing && numberIsMissing) {
+  console.log("enter a correct non-empty number");
+  process.exit(1);
+}
+
+const username = process.env.PHONE_DB_USERNAME || "admin";
+const hostname =
+  process.env.PHONE_DB_HOSTNAME || "cluster0.ogeza62.mongodb.net";
+
+const url = `mongodb+srv://${username}:${password}@${hostname}/personApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (nameIsMissing) {
+  Person.find({}).then((value) => {
+    console.log(value);
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+}
