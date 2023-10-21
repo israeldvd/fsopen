0058ff0adbb64ae9c00cd373bdfcd76d99e32350
Israel David <israel-david@outlook.com.br>
2023-10-21 00:01:29 -0300

    test(blog-list): add totalLikes group and its first test

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -5,3 +5,21 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog equals the like of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(listWithOneBlog[0].likes);
+  });
+});
