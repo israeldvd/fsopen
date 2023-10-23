a6594fd58aa2ec8c94d048b4540585a840cbdb8b
Israel David <israel-david@outlook.com.br>
2023-10-23 20:23:29 -0300

    test(blog-list): add most-blog group of tests

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,8 @@
+const last = require("lodash.last");
+const toPairs = require("lodash.topairs");
+const maxBy = require("lodash.maxby");
+const countBy = require("lodash.countby");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -20,8 +25,22 @@ const favoriteBlog = (blogs) => {
   };
 };
 
+const mostBlogs = (blogs) => {
+  const authorsPostCount = countBy(blogs, "author");
+  const pairs = toPairs(authorsPostCount);
+
+  if (pairs.length === 0) {
+    return { author: "", blogs: 0 };
+  }
+
+  const [author, numOfBlogs] = maxBy(pairs, last);
+
+  return { author, blogs: numOfBlogs };
+};
+
 module.exports = {
   dummy,
   favoriteBlog,
+  mostBlogs,
   totalLikes,
 };
