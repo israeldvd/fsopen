ee3a875eb3133adc332023ed98a8aea4cc7c596a
Israel David <israel-david@outlook.com.br>
2023-10-26 00:09:59 -0300

    fix(blog-list): change environment variable

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const MONGODB_URI =
   process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGO_URI
+    ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
 const PORT = process.env.PORT;
