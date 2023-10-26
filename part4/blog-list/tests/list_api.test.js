5ec5fc193791a6678893bb6fc03d18c634feee99
Israel David <israel-david@outlook.com.br>
2023-10-26 00:30:40 -0300

    test(blog-list): add tests with supertest to check api

---
+++ part4/blog-list/tests/list_api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../src/app");
+
+const api = supertest(app);
+const api_url = "/api/blogs";
+
+const helper = require("../tests/test_helper");
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogList);
+});
+
+describe("blog list API", () => {
+  test("(response) is returned as json", async () => {
+    await api
+      .get(api_url)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("returns the correct amount of blog posts", async () => {
+    const response = await api.get(api_url);
+    expect(response.body).toHaveLength(helper.initialBlogList.length);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
