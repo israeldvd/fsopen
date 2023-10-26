b4104c810ab228039458fd4c1b3670af5bc079cd
Israel David <israel-david@outlook.com.br>
2023-10-25 23:42:52 -0300

    feat(blog-list): change URI config for test environment

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -3,7 +3,11 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv-expand").expand(require("dotenv").config());
 }
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGODB_URI;
+
 const PORT = process.env.PORT;
 
 module.exports = {
