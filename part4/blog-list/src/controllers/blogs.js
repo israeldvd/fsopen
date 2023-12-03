09f1028d22eebfb9ef38fdc315f9d839b0ac8139
Israel David <israel-david@outlook.com.br>
2023-12-02 22:36:03 -0300

    fix: implement error-handling on blog controller

--- part4/blog-list/src/controllers/blogs.js
+++ part4/blog-list/src/controllers/blogs.js
@@ -1,12 +1,17 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+
+    response.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
   if (!body || !("title" in body)) {
@@ -16,8 +21,12 @@ blogsRouter.post("/", async (request, response) => {
 
   const blog = new Blog(body);
 
-  const result = await blog.save();
-  response.status(201).json(result);
+  try {
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
