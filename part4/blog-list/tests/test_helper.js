2c8ec502494079f34999c38d8a887a31440c4ba1
Israel David <israel-david@outlook.com.br>
2023-12-09 17:01:22 -0300

    chore(blog-list): add test helpers for users endpoint

--- part4/blog-list/tests/test_helper.js
+++ part4/blog-list/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require("../src/models/blog");
+const User = require("../src/models/user");
 
 const initialBlogList = [
   {
@@ -16,6 +17,45 @@ const initialBlogList = [
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
     __v: 0,
+  },
+];
+
+const initialUsersList = [
+  {
+    name: "Alidia Jewes",
+    email: "ajewes0@huffingtonpost.com",
+    username: "ajewes0",
+    password: "lP2=r(5~AH3+qL",
+  },
+  {
+    name: "Vladamir Downe",
+    email: "vdowne1@bandcamp.com",
+    username: "vdowne1",
+    password: "yZ9+7VQGL&M}.",
+  },
+  {
+    name: "Carolin Buey",
+    email: "cbuey2@cargocollective.com",
+    username: "cbuey2",
+    password: "hD5>d/D+opS&mR",
+  },
+  {
+    name: "Richart Astin",
+    email: "rastin3@cdc.gov",
+    username: "rastin3",
+    password: "hT3/I\\U35~.F6?g",
+  },
+  {
+    name: "Dagny Tieraney",
+    email: "dtieraney4@webeden.co.uk",
+    username: "dtieraney4",
+    password: "lG5*OVE|",
+  },
+  {
+    name: "Matilde Boaler",
+    email: "mboaler5@cornell.edu",
+    username: "mboaler5",
+    password: "rU2=~sCKC",
   },
 ];
 
@@ -37,8 +77,15 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
+
 module.exports = {
   initialBlogList,
   nonExistingId,
   blogsInDb,
+  initialUsersList,
+  usersInDb,
 };
