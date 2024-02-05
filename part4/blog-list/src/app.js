48bc6086f38e73400223e8e94cc95504c1cc1172
Israel David <israel-david@outlook.com.br>
2024-02-05 11:21:01 -0300

    feat(blog-list): move login router to ahead of token extractor middleware

--- part4/blog-list/src/app.js
+++ part4/blog-list/src/app.js
@@ -16,10 +16,10 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/login", loginRoute);
 app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/login", loginRoute);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
