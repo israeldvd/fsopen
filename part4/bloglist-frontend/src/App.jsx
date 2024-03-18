84ba2465df7bc825dd5bccb5489930ceff0d817a
Israel David <israel-david@outlook.com.br>
2024-03-18 16:52:04 -0300

    feat(blog-list): change after-login feedback to display user's name

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -16,10 +16,8 @@ const App = () => {
       {user === null && <LoginForm setUser={setUser} />}
       {user !== null && (
         <>
-          <h2>Welcome!</h2>
-          <p>Check all available posts. You are now logged in.</p>
-
           <h2>blogs</h2>
+          <p>{user.name} logged in</p>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
