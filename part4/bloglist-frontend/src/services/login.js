1e69db6f9cb17b4608319bdfeb4d911c9a37397f
Israel David <israel-david@outlook.com.br>
2024-03-18 14:55:43 -0300

    chore(bloglist-frontend): define a login service

---
+++ part4/bloglist-frontend/src/services/login.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const resourceUrl = "/api/login"
+
+const login = async ({ username, password }) => {
+  const response = await axios.post(resourceUrl, { username, password })
+  return response.data
+}
+
+export default { login }
