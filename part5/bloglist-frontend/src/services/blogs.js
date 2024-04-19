6a31e7cacb4ac73afaa71bba4b5c9556d389067b
Israel David <israel-david@outlook.com.br>
2024-04-19 13:29:24 -0300

    chore(bloglist-frontend): add new functions to set token and create a post

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,26 @@
 import axios from "axios"
 const baseUrl = "/api/blogs"
 
+let authHeader = null
+
+const setToken = (/** @type {string} */ newToken) => {
+  authHeader = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then((response) => response.data)
 }
 
-export default { getAll }
+const create = async (
+  /** @type {{title: string, author: string, url: string}} */ newPost,
+) => {
+  const config = {
+    headers: { Authorization: authHeader },
+  }
+
+  const response = await axios.post(baseUrl, newPost, config)
+  return response.data
+}
+
+export default { getAll, create, setToken }
