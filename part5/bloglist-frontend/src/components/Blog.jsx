16f90970ce2f0c5e0584a1e2ea17c69c87f9a12a
Israel David <israel-david@outlook.com.br>
2024-12-23 11:51:10 -0300

    feat(bloglist-frontend): add control buttons to blog list

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,54 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author ? blog.author.name : "(unknown author)"}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  // visibility status (state, text and display mode)
+  const [visible, setVisible] = useState(false);
+  const showWhenVisible = { display: (visible) ? '' : 'none' }
+  const visibilityTextButton = (visible) ? 'hide' : 'details'
+
+  // styles
+  const blogStyle = {
+    border: 'solid',
+    borderRadius: '0.2rem',
+    borderWidth: '0.1rem',
+    padding: '20px',
+    marginBottom: '1rem',
+    width: '50%',
+    minHeight: '20px',
+    overflow: 'hidden'
+  }
+
+  const headerStyle = { fontWeight: '600', display: 'flex', justifyContent: 'space-between' }
+
+  // event handlers
+  // detail visiblity button
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  // like button
+  const giveLike = () => {
+    return;
+  }
+
+  return (
+    <div style={blogStyle}>
+      <section style={headerStyle}>
+        {blog.title} {blog.author ? blog.author.name : "(unknown author)"}
+        <button onClick={toggleVisibility}>
+          {visibilityTextButton}
+        </button>
+      </section>
+      <section style={showWhenVisible}>
+        <p>
+          {blog.url}
+        </p>
+        <p>
+          {blog.likes} <button onClick={giveLike}>like</button>
+        </p>
+      </section>
+    </div>
+  )
+}
 
 export default Blog
