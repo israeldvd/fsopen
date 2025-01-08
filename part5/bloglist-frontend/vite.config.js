f1af782e11c2a8c05d73971cbe57a356f5eacdfa
Israel David <israel-david@outlook.com.br>
2025-01-08 18:50:20 -0300

    chore(bloglist-frontend): update test setup

--- part5/bloglist-frontend/vite.config.js
+++ part5/bloglist-frontend/vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
@@ -14,7 +15,7 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
-    globals: true,
+    exclude: ["node_modules"],
     setupFiles: './testSetup.js'
   }
 })
